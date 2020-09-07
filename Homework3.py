fb0a5508f4c3f1c2fe13da1da9ec49d40160edc9
svmat <vibirau@gmail.com>
2020-09-07 17:12:40 -0500

    Update Homework3.py
    added function and for-loop

--- Homework3.py
+++ Homework3.py
@@ -8,29 +8,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def waitForElementVisibility(element_xpath):
+  return WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, element_xpath)
+
 # wait until the "see why we're worth it" button becomes visible
-see_why_we_re_worth_it_btn = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//div[@data-elem-id=1551635176282]/a')))
+see_why_we_re_worth_it_btn = waitForElementVisibility('//div[@data-elem-id=1551635176282]/a')
 see_why_we_re_worth_it_btn.click()
 
+image_elements = ['//div[@data-elem-id=1551738136013]', '//div[@data-elem-id=1551738610551]', '//div[@data-elem-id=1551738610551]', '//div[@data-elem-id=1551738391114]')
+for image_xpath in image_elements:
+     waitForElementVisibility(image_xpath)
 
 # find the mobile development element in areas of expertise and verify it's text and image presence
-mobile_development_text = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//div[@data-elem-id=1551735998271]')))
+mobile_development_text = waitForElementVisibility('//div[@data-elem-id=1551735998271]')
 assert mobile_development_text.text == "Mobile Development"
-mobile_development_image = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//div[@data-elem-id=1551738136013]')))
 
 # find the web development element in areas of expertise and verify it's text and image presence
-web_development_text = WebDriverWait (driver, 15).until(EC.visibility_of_element_located((By.XPATH, ' //div[@data-elem-id=1551735996289]')))
+web_development_text = waitForElementVisibility('//div[@data-elem-id=1551735996289]')
 assert web_development_text.text == "Web Development"
-web_development_image = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//div[@data-elem-id=1551738610551]')))
 
 # find the augmented reality element in areas of expertise and verify it's text and image presence
-augmented_reality_text = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//div[@data-elem-id=1551735997222]')))
+augmented_reality_text = waitForElementVisibility('//div[@data-elem-id=1551735997222]')
 assert augmented_reality_text.text == "Augmented Reality"
-augmented_reality_image = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//div[@data-elem-id=1551738610551]')))
 
 # find the virtual reality element in the areas of expertise and verify it's text and image presence
-virtual_reality_text = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//div[@data-elem-id=1551735995307]')))
+virtual_reality_text = waitForElementVisibility('//div[@data-elem-id=1551735995307]')
 assert virtual_reality_text.text == "Virtual Reality"
-virtual_reality_image = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//div[@data-elem-id=1551738391114]')))
 
 driver.close()
